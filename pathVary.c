680b42d0fa83d3cc82432ad6ea7453f2ac4bb970
misaka-mikoto <sisnow@qq.com>
2013-01-25 04:29:30 -0500

    final module

--- pathVary.c
+++ pathVary.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <syslib.h>
 
 char *bkDir;
 bkDir="/usr/local/share/bkx_backup_directory";
 
-int varyAdd(char *filePath)
+char *varyAdd(char *filePath)
 {
-	
+	static char cachePath[]=bkDir;
+	strcat(cachePath,filePath);
+	return cachePath;
 }
 
 int varySub(char *filePath)
