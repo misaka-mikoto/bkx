1bc037466bfbf4859601a358e94c33f1327326f7
misaka-mikoto <sisnow@qq.com>
2013-01-24 20:26:11 -0500

    add the bkDir code

--- error.c
+++ error.c
@@ -12,7 +12,7 @@ void ERROR(int errNum)
 			printf("Please don't input more then one parameter, or input 'bkx --help' for help.");
 			break;
 		case 3:
-			printf("I don't have permission to read the target file!");
+			printf("Plz use super user!");
 			break;
 		case 4:
 			printf("I don't support directory backup.");
@@ -22,6 +22,13 @@ void ERROR(int errNum)
 			break;
 		case 6:
 			printf("Copy Error!");
+			break;
+		case 7:
+			printf("Backup directory error! Plz use super user.");
+			break;
+		case 8:
+			printf("Please create the backup file directory yourself.(/usr/local/share/bkx_backup_directory)");
+			break;
 		}
 	printf("\n");
 }
--- savex.c
+++ savex.c
@@ -2,10 +2,18 @@
 
 int saveBK(char *routeChar)
 {
+	char *bkDir;
+	bkdir="/usr/local/share/bkx_backup_directory";
 	if(!isFileExist(routeChar))
 		ERROR(5);
+	else if(isFileCanRead(routeChar))
+		ERROR(3);
 	else if(isDirectory(routeChar))
 		ERROR(4);
+	else if(!isFileExist(bkDir))
+		ERROR(8);
+	else if(!isDirectory(bkDir)||!isFileCanWrite(bkDir))
+		ERROR(7);
 	else
 	{
 		printf("ok");
