92cb1aedacb4025b2b017eb3cb398e492e027be8
misaka-mikoto <sisnow@qq.com>
2013-01-24 20:37:17 -0500

    fix something

--- savex.c
+++ savex.c
@@ -3,10 +3,10 @@
 int saveBK(char *routeChar)
 {
 	char *bkDir;
-	bkdir="/usr/local/share/bkx_backup_directory";
+	bkDir="/usr/local/share/bkx_backup_directory";
 	if(!isFileExist(routeChar))
 		ERROR(5);
-	else if(isFileCanRead(routeChar))
+	else if(!isFileCanRead(routeChar))
 		ERROR(3);
 	else if(isDirectory(routeChar))
 		ERROR(4);
